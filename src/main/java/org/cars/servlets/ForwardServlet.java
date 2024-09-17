c5feeb4f3c2ef380525e07afae6007cf10ffeda2
reallygoodsir <tymur5512@gmail.com>
2024-09-17 21:12:13 +0300

    added forward

---
+++ src/main/java/org/cars/servlets/ForwardServlet.java
@@ -0,0 +1,17 @@
+package org.cars.servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ForwardServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/file-upload");
+        requestDispatcher.forward(request, response);
+    }
+}
