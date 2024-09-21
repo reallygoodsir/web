625f23285af96a404803d39f7c3c5fdc4f441cef
reallygoodsir <tymur5512@gmail.com>
2024-09-21 15:44:28 +0300

    added servlet config

--- src/main/java/org/cars/servlets/PageRedirectServlet.java
+++ src/main/java/org/cars/servlets/PageRedirectServlet.java
@@ -1,5 +1,6 @@
 package org.cars.servlets;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,10 +10,11 @@ public class PageRedirectServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
         ServletContext servletContext = getServletContext();
+        ServletConfig servletConfig = getServletConfig();
+        String footballRedirectUrl = servletConfig.getInitParameter("football-redirect-url");
         servletContext.log("Start PageRedirectServlet redirection");
-        String site = "https://football.ua/";
         response.setStatus(response.SC_MOVED_TEMPORARILY);
-        response.setHeader("Location", site);
+        response.setHeader("Location", footballRedirectUrl);
         servletContext.log("End PageRedirectServlet redirection");
     }
 }
